4044a5941aedbcb977ab8b6f8f42ea482b4b91c3
Georgi Rashkov <grashcov@gmail.com>
2024-09-25 09:27:22 +0300

    feat: Mptt, spectacular, serializers and views added

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
+from .models import ProductModel
+from .views import CategoryView
+
+router = routers.DefaultRouter()
+router.register(r'categories', CategoryView,basename='category')
 urlpatterns = [
-
+    path("", include(router.urls))
 ]
